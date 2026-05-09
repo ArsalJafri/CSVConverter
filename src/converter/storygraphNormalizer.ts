fe1768228894fa9579b3eedfb48a55841a28b9d1
Arsal <arsaljafri@gmail.com>
2026-05-08 21:06:08 -0500

    added more function

--- src/converter/storygraphNormalizer.ts
+++ src/converter/storygraphNormalizer.ts
@@ -1,3 +1,7 @@
 import type { RawStoryGraphRow, NormalizedBook } from './types';
 
-function splitStringIntoList(rawString: string) : string[] {return rawString.split(",").map(item => item.trim()).filter(word => word.length > 0)}
+function splitStringToList(rawString: string) : string[] {return rawString.split(",").map(item => item.trim()).filter(word => word.length > 0)}
+
+function parseOptionalNumber(rawNumber: string) : number | undefined { return !isNaN(+rawNumber) && rawNumber.trim().length !== 0 ? +rawNumber : undefined }
+
+ 
