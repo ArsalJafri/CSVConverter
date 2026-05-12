2b7e47ec0407826902c03f153e354783c30cfc5d
Arsal <arsaljafri@gmail.com>
2026-05-11 21:33:48 -0500

    Adding some more functions to work with

--- src/converter/storygraphNormalizer.ts
+++ src/converter/storygraphNormalizer.ts
@@ -2,6 +2,17 @@ import type { RawStoryGraphRow, NormalizedBook } from './types';
 
 function splitStringToList(rawString: string) : string[] {return rawString.split(",").map(item => item.trim()).filter(word => word.length > 0)}
 
-function parseOptionalNumber(rawNumber: string) : number | undefined { return !isNaN(+rawNumber) && rawNumber.trim().length !== 0 ? +rawNumber : undefined }
+function parseValidNumber(rawStringNumber: string) : number | undefined { return !isNaN(+rawStringNumber) && rawStringNumber.trim().length !== 0 ? +rawStringNumber : undefined }
 
- 
+function parseReadCount(rawStringNumber: string) : number {return +rawStringNumber > 0 && !isNaN(+rawStringNumber) && Number.isInteger(+rawStringNumber) ? +rawStringNumber : 0}
+
+function isOwned(ownedText: string) : boolean {return ownedText.trim().toLowerCase() === "yes"}
+
+function validateReadStatus(status: string) : string {
+    const listOfStatus: string[] = ["read", "to-read", "did-not-finish", "paused", "currently-reading"]
+
+    return listOfStatus.includes(status) ? status: "unknown"
+}
+
+function normalizeReadStatus(bookStatus: string) : string { return validateReadStatus(bookStatus.trim().toLowerCase())}
+
